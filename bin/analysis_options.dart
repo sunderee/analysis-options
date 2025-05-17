c38e8a6260c962c0cd6c1a565b98723b9076bc63
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-05-17 08:39:35 +0200

    Refactor analysis options to use command-line arguments for file path and style; add CLI argument parsing functionality

--- bin/analysis_options.dart
+++ bin/analysis_options.dart
@@ -1,13 +1,14 @@
-import 'package:analysis_options/data/style.dart';
+import 'package:analysis_options/cli.dart';
 import 'package:analysis_options/services/api_service.dart';
 import 'package:analysis_options/services/yaml_service.dart';
 
 Future<void> main(List<String> arguments) async {
+  final parsedArguments = parseArguments(arguments);
   final analysisRules = await fetchAnalysisRules();
 
   await writeAnalysisFile(
-    '/Users/peteraleksanderbizjak/Documents/private/tooling/analysis_options/analysis_options.yaml',
-    Style.recommended,
+    parsedArguments.filePath,
+    parsedArguments.style,
     analysisRules,
   );
 }
---
+++ lib/cli.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:analysis_options/data/style.dart';
+import 'package:args/args.dart';
+
+typedef ParsedArguments = ({String filePath, Style style});
+
+ParsedArguments parseArguments(List<String> arguments) {
+  final parser = ArgParser();
+  parser.addOption(
+    'path',
+    abbr: 'p',
+    mandatory: true,
+    help: 'Absolute path to the analysis_options.yaml file',
+  );
+  parser.addOption(
+    'style',
+    abbr: 's',
+    mandatory: true,
+    allowed: Style.values.map((item) => item.name),
+  );
+  parser.addFlag('help', abbr: 'h', help: 'Show the usage syntax.');
+
+  try {
+    final result = parser.parse(arguments);
+    if (result.wasParsed('help')) {
+      print(parser.usage);
+      exit(0);
+    }
+
+    String filePath = '';
+    Style styleToUse = Style.core;
+    if (result.wasParsed('path')) {
+      final path = result['path'] as String;
+      if (File(path).existsSync()) {
+        filePath = path;
+      } else {
+        print('File does not exist');
+        exit(1);
+      }
+    } else {
+      print(parser.usage);
+      exit(1);
+    }
+
+    if (result.wasParsed('style')) {
+      final style = result['style'] as String;
+      if (Style.values.map((item) => item.name).contains(style)) {
+        styleToUse = Style.values.firstWhere((item) => item.name == style);
+      }
+    } else {
+      print(parser.usage);
+      exit(1);
+    }
+
+    return (filePath: filePath, style: styleToUse);
+  } catch (error) {
+    print(error);
+    exit(1);
+  }
+}
